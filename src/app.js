bf874d5d5f06193f652713672f2e36baacdd2dd5
miclon <jcnd@163.com>
2022-07-30 13:03:02 +0800

    ✨ feat: router extractor

--- src/app.js
+++ src/app.js
@@ -1,30 +1,31 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import render from './controller.js'
+import { render, extractor } from './controllers/index.js'
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
-//configure body-parser for express
-app.use(bodyParser.urlencoded({extended:false}))
+// configure body-parser for express
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 
 // Error page.
 app.use((err, req, res, next) => {
-  console.error(err)
-  res.status(500).send(err)
+    console.error(err)
+    res.status(500).send(err)
 })
 // render route.
 app.post('/render', render)
+// extractor route.
+app.post('/extractor', extractor)
 
 app.listen(port, () => {
-  console.info(`Listen port on ${port}.`)
+    console.info(`Listen port on ${port}.`)
 })
 
 // Terminate process
 process.on('SIGINT', () => {
-  process.exit(0)
+    process.exit(0)
 })
 
